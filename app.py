19c81f52a710badb42faab65ccf269bf913253eb
MahmoudRamdan09 <123075703+MahmoudRamdan09@users.noreply.github.com>
2025-04-15 11:07:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -94,8 +94,8 @@ def predict():
 
 # Run the Flask application
 if __name__ == '__main__':
-port = int(os.environ.get('PORT', 5000))  
-    app.run(debug=False, host='0.0.0.0', port=port)  
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
 
 # # Prediction Endpoint
 # Route: /predict accepts POST requests.
